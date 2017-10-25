41add5280bf23c70058ce39b7aded33f1626e484
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-10-25 10:32:27 -0300

    burbuja

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,5 +4,17 @@
 
 template <class T>
 void bubbleSort(T *vect, unsigned long tamanio){
-
+    unsigned long i;
+    int flag;
+    T aux;
+    do{
+        flag=0;
+        for(i=0; i<tamanio-1; i++)
+            if(vect[i]>vect[i+1]){
+                aux=vect[i];
+                vect[i]=vect[i+1];
+                vect[i+1]=aux;
+                flag=1;
+            }
+    }while(flag==1);
 }
