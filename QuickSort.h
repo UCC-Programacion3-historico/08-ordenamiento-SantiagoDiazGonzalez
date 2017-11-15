ae1bddf356a6a51b90f45a60de114e30dcc66384
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-11-15 02:55:15 -0300

    quicksort y shellsort

--- QuickSort.h
+++ QuickSort.h
@@ -4,7 +4,32 @@
 
 template <class T>
 void quickSort(T *vect, unsigned long inicio, unsigned long fin){
+    unsigned long i, j, central;
 
+    central = (inicio+fin)/2;
+    T pivote = vect[central];
+    i = inicio;
+    j = fin;
+
+    do{
+        while (vect[i] < pivote) i++;
+        while (vect[j] > pivote) j--;
+
+        if(i <= j)
+        {
+            T aux = vect[i];
+            vect[i]=vect[j];
+            vect[j]=aux;
+            i++;
+            j--;
+        }
+    }
+    while (i <= j);
+
+    if (inicio < j)
+        quickSort(vect, inicio, j);
+    if (i < fin)
+        quickSort(vect, i, fin);
 }
 
 
--- ShellSort.h
+++ ShellSort.h
@@ -3,8 +3,26 @@
 
 
 template <class T>
-void shellSort(T *vect, unsigned long tamanio);
-
-
+void shellSort(T *vect, unsigned long tamanio){
+    int salto, i, j, k;
+    salto = tamanio/2;
+    while(salto > 0){
+        for (i = salto; i < tamanio ;i++){
+            j = i - salto;
+            while(j>=0){
+                k = j+salto;
+                if (vect[j] <= vect[k])
+                    j=-1;
+                else{
+                    T aux = vect[j];
+                    vect[j]=vect[j+1];
+                    vect[j+1]=aux;
+                    j-=salto;
+                }
+            }
+        }
+        salto=salto/2;
+    }
+}
 
 #endif //SHELLSORT_H
